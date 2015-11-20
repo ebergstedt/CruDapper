a01fccba8c5d47859ef2a50ee568949317d51faa
Erik Bergstedt <eb9418@redbull.xdin.com>
2015-11-20 12:09:29 +0100

    Easy provider switching

--- CruDapper.Test/BaseService.cs
+++ CruDapper.Test/BaseService.cs
@@ -1,4 +1,5 @@
-﻿using CruDapper.BackofficeTest;
+﻿using System;
+using CruDapper.BackofficeTest;
 using CruDapper.Infrastructure;
 using CruDapper.Mappers;
 using CruDapper.Services;
@@ -15,9 +16,19 @@ namespace CruDapper.Test
         public BaseService()
         {
             Provider = Provider.MsSql; //for test methods
-            DbMapper = new MsSqlServerMapper("DefaultConnection");
-            ServiceFactory = new ServiceFactory(DbMapper);
 
+            switch (Provider)
+            {
+                case Provider.MsSql:
+                    DbMapper = new MsSqlServerMapper("DefaultConnection");
+                    break;
+                case Provider.Postgres:
+                    DbMapper = new PostgresMapper("Postgres");
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }            
+            ServiceFactory = new ServiceFactory(DbMapper);
             CrudService = ServiceFactory.Get<CrudService>();
         }
 
